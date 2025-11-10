0f787ddaabb267b982f4ad7845af04b1560343c5
Muneeb Aziz <muneebazizmughal@gmail.com>
2025-11-10 12:39:10 +0500

    fix: correct InteriorRenderings import path/case

--- client/src/Pages/Interior.jsx
+++ client/src/Pages/Interior.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Interiorrenderings from '../Components/Gallery/interiorrenderings'
+import Interiorrenderings from '../Components/Gallery/Interiorrenderings'
 
 const Interior = () => {
   return (
     <div>
-        <Interiorrenderings/>
+       <Interiorrenderings/>
     </div>
   )
 }
